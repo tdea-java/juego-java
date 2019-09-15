70a3b37a13ffd13e42b7ee7108abdf6222cc3cb7
Andres <sernmandres@gmail.com>
2019-09-15 10:15:02 -0500

    identifadores de 0-5 en los tableros

--- batallaNaval.java
+++ batallaNaval.java
@@ -166,8 +166,11 @@ public class batallaNaval {
           if(turnoPlayer){
              mensajes(tablero2,coorX,coorY);
           }
+          System.out.print("y/x\t0\t1\t2\t3\t4\t5\n");
           for(int i = 0; i < tableroVPlayer1.length; i++){
+            System.out.print( i +"\t");
               for(int j = 0; j < tableroVPlayer1[i].length; j++){
+
                   System.out.print(tableroVPlayer1[i][j] + "\t");
               }
               System.out.println();
@@ -178,8 +181,9 @@ public class batallaNaval {
            if(!turnoPlayer){
              mensajes(tablero1,coorX,coorY);
           }
-
+          System.out.print("y/x\t0\t1\t2\t3\t4\t5\n");
           for(int i = 0; i < tableroVPlayer2.length; i++){
+            System.out.print( i +"\t");
               for(int j = 0; j < tableroVPlayer2[i].length; j++){
                   System.out.print(tableroVPlayer2[i][j] + "\t");
               }
